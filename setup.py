37a797abe127ae4902e84abc1e1efc66f61d76df
fancyxun <837633751@qq.com>
2022-06-06 10:17:04 +0800

    add sdk

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 # Fields marked as "Optional" may be commented out.
 
 setup(
-    name='points-federated',  # Required
+    name='EulerDB',  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     # For a discussion on single-sourcing the version across setup.py and the
@@ -52,9 +52,9 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
     ],
-    keywords='tensorflow_federated, setuptools, development',  # Optional
-    package_dir={'': 'tensorflow_federated'},  # Optional
-    packages=find_packages(where='tensorflow_federated'),  # Required
+    keywords='EulerDB, setuptools, development',  # Optional
+    package_dir={'': 'EulerDB'},  # Optional
+    packages=find_packages(where='EulerDB'),  # Required
     python_requires='>=3.6, <4',
 
     # This field lists other packages that your project depends on to run.
@@ -63,43 +63,12 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["rsa~=4.6.0",
-                      "absl-py~=0.10.0",
-                      "attrs~=18.2",
-                      "cachetools~=3.1.1",
-                      "dm-tree~=0.1.1",
-                      "enum34~=1.1",
-                      "grpcio~=1.29.0",
-                      "grpcio-tools~=1.29.0",
-                      "gmpy2~=2.0.8",
-                      "h5py~=2.6",
-                      "numpy~=1.18.0",
-                      "pandas~=0.24.0",
-                      "portpicker~=1.3.1",
-                      "retrying~=1.3.3",
-                      "six~=1.12.0",
-                      "tensorflow~=2.3.0",
-                      "tensorflow-model-optimization~=0.1.3",
-                      "tensorflow-privacy~=0.2.0",
-                      "tensorflow-addons~=0.8.0",
-                      "tensorflow_datasets~=2.0.0",
-                      "typing~=3.7.0",
-                      "pyopenssl~=19.1.0",
-                      "pycrypto~=2.6.1",
-                      "scikit-learn~=0.20.0",
-                      "utilitybelt~=0.2.6",
-                      "pycrypto~=2.6.1",
-                      "pymysql~=0.9.3",
-                      "phe~=1.4.0",
-                      "SQLAlchemy~=1.3.17",
-                      "psutil~=5.7.0",
-                      "progressbar~=2.5",
-                      "Pillow~=6.2.0",
-                      "numba~=0.50.1",
-                      "gmssl~=3.2.1",
-                      "cryptography~=3.4.7",
-                      "pathos~=0.2.7",
-                      "pyspark==3.0.1"],
+    install_requires=["mysql-connector-python == 8.0.28",
+                      "gmssl == 3.2.1",
+                      "cryptography == 36.0.1",
+                      "Crypto == 1.4.1",
+                      "tornado"
+                      "phe == 1.4.0"],
     extras_require={  # Optional
         'dev': ['check-manifest'],
         'test': ['coverage'],
